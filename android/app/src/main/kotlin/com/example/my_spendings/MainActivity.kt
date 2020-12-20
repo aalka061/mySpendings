2274abf101acb35739008f6185f5bbca1f0c69c0
abdullah alkaf <aalkaf@smatbuildingapps.com>
2020-12-20 13:09:00 -0500

    init

---
+++ android/app/src/main/kotlin/com/example/my_spendings/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_spendings
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter app"),
+      ),
+      body: Center(
+        child: Text("wdiget playground"),
+      ),
+    );
+  }
+}
