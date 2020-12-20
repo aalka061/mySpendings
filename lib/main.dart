48dc08bd6c15363f308aead3d560ffdb1bf5526c
abdullah alkaf <aalkaf@smatbuildingapps.com>
2020-12-20 13:28:55 -0500

    combining main widgets

--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,30 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter app"),
-      ),
-      body: Center(
-        child: Text("wdiget playground"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Flutter app"),
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 60,
+              child: Card(
+                child: Center(child: Text("Chart")),
+                elevation: 6,
+                shadowColor: Colors.blue,
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+              child: Card(
+                child: Text('List of expenses'),
+                elevation: 6,
+                shadowColor: Colors.blue,
+              ),
+            ),
+          ],
+        ));
   }
 }
