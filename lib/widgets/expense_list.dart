f3bff58dc3fe74873f573b0655a2ac8775fe690a
AAK <alkafabd@gmail.com>
2020-12-30 00:21:14 -0500

    wrap up

--- lib/widgets/expense_list.dart
+++ lib/widgets/expense_list.dart
@@ -11,7 +11,7 @@ class ExpenseList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 500,
       child: expenses.isEmpty
           ? Column(
               children: [
